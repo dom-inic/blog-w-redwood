ac7ba4c8100232ff6a98422340c201b6fe8ac076
dom-inic <dominicnyambane22@gmail.com>
2022-08-11 12:29:16 +0300

    post scaffold

---
+++ api/src/services/posts/posts.scenarios.js
@@ -0,0 +1,6 @@
+export const standard = defineScenario({
+  post: {
+    one: { data: { title: 'String1830826', body: 'String' } },
+    two: { data: { title: 'String9088133', body: 'String' } },
+  },
+})
---
+++ api/src/services/posts/posts.test.js
@@ -0,0 +1,47 @@
+import { posts, post, createPost, updatePost, deletePost } from './posts'
+
+// Generated boilerplate tests do not account for all circumstances
+// and can fail without adjustments, e.g. Float and DateTime types.
+//           Please refer to the RedwoodJS Testing Docs:
+//       https://redwoodjs.com/docs/testing#testing-services
+// https://redwoodjs.com/docs/testing#jest-expect-type-considerations
+
+describe('posts', () => {
+  scenario('returns all posts', async (scenario) => {
+    const result = await posts()
+
+    expect(result.length).toEqual(Object.keys(scenario.post).length)
+  })
+
+  scenario('returns a single post', async (scenario) => {
+    const result = await post({ id: scenario.post.one.id })
+
+    expect(result).toEqual(scenario.post.one)
+  })
+
+  scenario('creates a post', async () => {
+    const result = await createPost({
+      input: { title: 'String3369995', body: 'String' },
+    })
+
+    expect(result.title).toEqual('String3369995')
+    expect(result.body).toEqual('String')
+  })
+
+  scenario('updates a post', async (scenario) => {
+    const original = await post({ id: scenario.post.one.id })
+    const result = await updatePost({
+      id: original.id,
+      input: { title: 'String24963502' },
+    })
+
+    expect(result.title).toEqual('String24963502')
+  })
+
+  scenario('deletes a post', async (scenario) => {
+    const original = await deletePost({ id: scenario.post.one.id })
+    const result = await post({ id: original.id })
+
+    expect(result).toEqual(null)
+  })
+})
