00da14656cec6be2c5870ab5a2062a43750da6fd
dom-inic <dominicnyambane22@gmail.com>
2022-08-12 12:26:14 +0300

    cell for data loading, states, render data

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,11 +1,13 @@
 // import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
+import ArticlesCell from 'src/components/ArticlesCell'
+
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-      Home
+      <ArticlesCell />
     </>
   )
 }
