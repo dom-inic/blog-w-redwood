443d6de196b0f6fe80073c630efc15bb110c8c2b
dom-inic <dominicnyambane22@gmail.com>
2022-08-13 13:46:58 +0300

    contact form with validation

--- web/src/pages/ArticlePage/ArticlePage.js
+++ web/src/pages/ArticlePage/ArticlePage.js
@@ -1,6 +1,7 @@
 import { MetaTags } from '@redwoodjs/web'
 
 import ArticleCell from 'src/components/ArticleCell'
+import CommentCell from 'src/components/Comment/CommentCell'
 
 const ArticlePage = ({ id }) => {
   return (
@@ -8,6 +9,7 @@ const ArticlePage = ({ id }) => {
       <MetaTags title="Article" description="Article page" />
 
       <ArticleCell id={id} />
+      <CommentCell />
     </>
   )
 }
--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,13 +1,78 @@
+import {
+  Form,
+  TextField,
+  TextAreaField,
+  Submit,
+  FieldError,
+  Label,
+} from '@redwoodjs/forms'
 import { MetaTags } from '@redwoodjs/web'
 
-import ContactCell from 'src/components/Contact/ContactCell'
-
 const ContactPage = () => {
+  const onSubmit = (data) => {
+    console.log(data)
+  }
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
+      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }} >
+        <Label htmlFor="firstname" errorClassName="error">
+          First Name
+        </Label>
+        <TextField
+          name="firstname"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="firstname" className="error" />
 
-      <ContactCell />
+        <Label htmlFor="secondname" errorClassName="error">
+          Second Name
+        </Label>
+        <TextField
+          name="secondname"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="secondname" className="error" />
+
+        <Label htmlFor="email" errorClassName="error">
+          Email
+        </Label>
+        <TextField
+          name="email"
+          validation={{
+            required: true,
+            pattern: {
+              value: /^[^@]+@[^.]+\..+$/,
+              message: 'Please enter a valid email address',
+            },
+          }}
+          errorClassName="error"
+        />
+        <FieldError name="email" className="error" />
+
+        <Label htmlFor="subject" errorClassName="error">
+          Subject
+        </Label>
+        <TextField
+          name="subject"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="subject" className="error" />
+
+        <Label htmlFor="message" errorClassName="error">
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="message" className="error" />
+        <Submit>Save</Submit>
+      </Form>
     </>
   )
 }
